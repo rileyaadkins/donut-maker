40d6ce5283718fb5b16c226738f3e59e9f88635c
Riley Adkins <rileyaadkins@gmail.com>
2020-07-02 16:57:27 -0400

    auto clicker working

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 const appDoc = document.getElementById("app");
 
 // counter
-let count = 0;
+let count = 330;
+let donutMultiplierCount = 0;
+let autoClickerPrice = 100;
+let autoClickerCount = 0;
+
+const multiplierPriceDiv = document.querySelector(".multiplier-price");
+const multiplierCountDiv = document.querySelector(".multiplier-count");
+const autoClickerCountDiv = document.querySelector(".auto-clicker-count");
+const autoClickerPriceDiv = document.querySelector(".auto-clicker-price");
 
 //button elements
 const donutButton = document.getElementById("donut-button");
-const autoClicker = document.getElementById("auto-clicker");
+const autoClicker = document.getElementById("auto-clicker-button");
 
 //donut display
 const displayContainer = document.getElementById("donuts");
 displayContainer.innerText = `Donut Count: ${count}`;
 
 //donut counter click handler
-donutButton.onclick = function () {
-  count++;
+donutButton.addEventListener("click", function () {
+  count += Math.pow(1.2, donutMultiplierCount);
   displayContainer.innerText = `Donut Count: ${count}`;
-};
+});
 
-// auto-clicker handler
-autoClicker.onclick = function () {};
+// auto clicker button
+autoClicker.addEventListener("click", buyAutoClicker);
+
+// auto clicker function
+function buyAutoClicker() {
+  if (count >= autoClickerPrice) {
+    count -= autoClickerPrice;
+    autoClickerCount += 1;
+    autoClickerPrice = Math.round(autoClickerPrice * 1.1);
+    displayContainer.innerText = `Donut Count: ` + count;
+    autoClickerCountDiv.innerText = `Purchased: ` + autoClickerCount;
+    autoClickerPriceDiv.innerText = `Price: ` + autoClickerPrice;
+    if (autoClickerCount <= 1) {
+      activateAutoClickers();
+    }
+  }
+}
+
+function activateAutoClickers() {
+  setInterval(function () {
+    count += autoClickerCount * Math.pow(1.2, donutMultiplierCount);
+    displayContainer.innerText = `Donut Count: ` + Math.round(count);
+  }, 1000);
+}

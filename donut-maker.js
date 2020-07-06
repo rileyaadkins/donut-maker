f353991f76bcfebc8347e8c41e291dc8f5ea0510
Riley Adkins <rileyaadkins@gmail.com>
2020-07-06 09:34:39 -0400

    morning commit

--- donut-maker.js
+++ donut-maker.js
@@ -86,3 +86,20 @@ resetButton.addEventListener("click", resetDonutCount);
 function resetDonutCount() {
   location.reload();
 }
+
+// menu collapse
+
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
